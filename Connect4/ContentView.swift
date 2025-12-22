1876bf0e3c1c1e2afc532ae7d929da3b29dbbe58
Jaxcho <jaxcho1118@gmail.com>
2025-12-21 23:30:29 -0800

    Initial Commit

---
+++ Connect4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Connect4
+//
+//  Created by Jax Choi on 12/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
