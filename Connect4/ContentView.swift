d776419ac518a3d46904111f63999b872ecf8898
Jaxcho <jaxcho1118@gmail.com>
2026-01-04 21:21:11 -0800

    changes

--- Connect4/ContentView.swift
+++ Connect4/ContentView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var scores: [Int]
+    
+    init() {
+        _scores = State(initialValue: [0])
+    }
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .font(.largeTitle)
+            
+            // Fix typo: use String initializer instead of Str
+            Text(String(scores[0]))
+            Button("Add 90") {
+                scores.append(90)
+            }
         }
         .padding()
     }
